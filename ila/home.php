c2e7c0348d3c25ea9cb70b210b94f83016a98766
Ngoc Hai <duongngochai3000@gmail.com>
2018-04-03 15:58:36 +0700

    edit file home.php

--- ila/home.php
+++ ila/home.php
@@ -76,7 +76,7 @@ $( function() {
 						<label for="radio-1">Animal</label>
 						<input type="radio" name="radio-1" id="radio-1">
 						<label for="radio-2">Body</label>
-						<input type="radio" name="radio-2 id="radio-2">
+						<input type="radio" name="radio-2" id="radio-2">
 						<label for="radio-3">Body Care</label>
 						<input type="radio" name="radio-1" id="radio-3">
 						<label for="radio-4">Body Care</label>
