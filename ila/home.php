061ea17553bfb7bd7052f868a5a5999b3397333f
Ngoc Hai <duongngochai3000@gmail.com>
2018-04-03 11:06:57 +0700

    edit navbar-hearder

--- ila/home.php
+++ ila/home.php
@@ -39,10 +39,11 @@ $( function() {
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Contents</a>
+          <a class="navbar-brand" href="#">KMITS</a>
         </div><!-- /navbar-header -->
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
+          	<li><a href="#">Contents</a>
             <li><a href="#">Documents</a></li>
             <li class="active"><a href="#">Vocabulary</a></li>
             <li><a href="#">Units</a></li>
