379749e2dd4025f4571589a3255228121be0854b
Ngoc Hai <duongngochai3000@gmail.com>
2018-04-09 17:28:30 +0700

    add new category transportation for database

--- ila/home.php
+++ ila/home.php
@@ -73,82 +73,45 @@ $( function() {
 	    		<h6>Learning Vocabulary by Pictures!</h6>
 					<fieldset>
 						<legend>Select a Category: </legend>
-						<label for="radio-1">Animal</label>
-						<input type="radio" name="radio-1" id="radio-1">
-						<label for="radio-2">Body</label>
-						<input type="radio" name="radio-2" id="radio-2">
-						<label for="radio-3">Body Care</label>
-						<input type="radio" name="radio-3" id="radio-3">
-						<label for="radio-4">Body Care</label>
-						<input type="radio" name="radio-4" id="radio-4">
-						<label for="radio-5">Books and Things to Read</label>
-						<input type="radio" name="radio-5" id="radio-5">
-						<label for="radio-6">Buildings</label>
-						<input type="radio" name="radio-6" id="radio-6">
-						<label for="radio-7">Business English</label>
-						<input type="radio" name="radio-7" id="radio-7">
-						<label for="radio-8">Calendar</label>
-						<input type="radio" name="radio-8" id="radio-8">
-						<label for="radio-9">Cars</label>
-						<input type="radio" name="radio-9" id="radio-9">
-						<label for="radio-10">Celebrations</label>
-						<input type="radio" name="radio-10" id="radio-10">
-						<label for="radio-11">City</label>
-						<input type="radio" name="radio-11" id="radio-11">
-						<label for="radio-12">Clothers</label>
-						<input type="radio" name="radio-12" id="radio-12">
-						<label for="radio-13">Colors</label>
-						<input type="radio" name="radio-13" id="radio-13">
-						<label for="radio-14">Computers</label>
-						<input type="radio" name="radio-14" id="radio-14">
-						<label for="radio-15">Countries</label>
-						<input type="radio" name="radio-15" id="radio-15">
-						<label for="radio-16">Family</label>
-						<input type="radio" name="radio-16" id="radio-16">
-						<label for="radio-17">Eating, Food | Drinks</label>
-						<input type="radio" name="radio-17" id="radio-17">
-						<label for="radio-18">Gardening and Plants</label>
-						<input type="radio" name="radio-18" id="radio-18">
-						<label for="radio-19">Geography</label>
-						<input type="radio" name="radio-19" id="radio-19">
-						<label for="radio-20">Health</label>
-						<input type="radio" name="radio-20" id="radio-20">
-						<label for="radio-21">Holidays</label>
-						<input type="radio" name="radio-21" id="radio-21">
-						<label for="radio-22">House</label>
-						<input type="radio" name="radio-22" id="radio-22">
-						<label for="radio-23">Jobs, Occupations and Professions</label>
-						<input type="radio" name="radio-23" id="radio-23">
-						<label for="radio-24">Law</label>
-						<input type="radio" name="radio-24" id="radio-24">
-						<label for="radio-25">Miscellaneous</label>
-						<input type="radio" name="radio-25" id="radio-25">
-						<label for="radio-26">Money</label>
-						<input type="radio" name="radio-26" id="radio-26">
-						<label for="radio-27">Music</label>
-						<input type="radio" name="radio-27" id="radio-27">
-						<label for="radio-28">Office</label>
-						<input type="radio" name="radio-28" id="radio-28">
-						<label for="radio-29">People</label>
-						<input type="radio" name="radio-29" id="radio-29">
-						<label for="radio-30">Seasons</label>
-						<input type="radio" name="radio-30" id="radio-30">
-						<label for="radio-31">Sports</label>
-						<input type="radio" name="radio-31" id="radio-31">
-						<label for="radio-32">Time</label>
-						<input type="radio" name="radio-32" id="radio-32">
-						<label for="radio-33">Transportation</label>
-						<input type="radio" name="radio-33" id="radio-33">
-						<label for="radio-34">Travel</label>
-						<input type="radio" name="radio-34" id="radio-34">
-						<label for="radio-35">Weather</label>
-						<input type="radio" name="radio-35" id="radio-35">
-						<label for="radio-36">Food</label>
-						<input type="radio" name="radio-36" id="radio-36">
-					</fieldset>
+							<button type="button" class="btn btn-primary btn-sm" id="1" value="Food">Food</button>
+							<button type="button" class="btn btn-primary btn-sm" id="2" value="Animal">Animal</button>
+							<button type="button" class="btn btn-primary btn-sm" id="3" value="Body">Body</button>
+							<button type="button" class="btn btn-primary btn-sm" id="4" value="Body Care">Body Care</button>
+							<button type="button" class="btn btn-primary btn-sm" id="5" value="Books and Things to Read">Books and Things to Read</button>
+							<button type="button" class="btn btn-primary btn-sm" id="6" value="Buildings">Buildings</button>
+							<button type="button" class="btn btn-primary btn-sm" id="7" value="Business English">Business English</button>
+							<button type="button" class="btn btn-primary btn-sm" id="8" value="Calendar">Calendar</button>
+							<button type="button" class="btn btn-primary btn-sm" id="9" value="Cars">Cars</button>
+							<button type="button" class="btn btn-primary btn-sm" id="10" value="Celebrations">Celebrations</button>
+							<button type="button" class="btn btn-primary btn-sm" id="11" value="City">City</button>
+							<button type="button" class="btn btn-primary btn-sm" id="12" value="Clothers">Clothers</button>
+							<button type="button" class="btn btn-primary btn-sm" id="13" value="Colors">Colors</button>
+							<button type="button" class="btn btn-primary btn-sm" id="14" value="Computers">Computers</button>
+							<button type="button" class="btn btn-primary btn-sm" id="15" value="Countries">Countries</button>
+							<button type="button" class="btn btn-primary btn-sm" id="16" value="Family">Family</button>
+							<button type="button" class="btn btn-primary btn-sm" id="17" value="Eating, Food | Drinks">Eating, Food | Drinks</button>
+							<button type="button" class="btn btn-primary btn-sm" id="18" value="Gardening and Plants">Gardening and Plants</button>
+							<button type="button" class="btn btn-primary btn-sm" id="19" value="Geography">Geography</button>
+							<button type="button" class="btn btn-primary btn-sm" id="20" value="Health">Health</button>
+							<button type="button" class="btn btn-primary btn-sm" id="21" value="Holidays">Holidays</button>
+							<button type="button" class="btn btn-primary btn-sm" id="22" value="House">House</button>
+							<button type="button" class="btn btn-primary btn-sm" id="23" value="Jobs, Occupations and Professions">Jobs, Occupations and Professions</button>
+							<button type="button" class="btn btn-primary btn-sm" id="24" value="Law">Law</button>
+							<button type="button" class="btn btn-primary btn-sm" id="25" value="Miscellaneous">Miscellaneous</button>
+							<button type="button" class="btn btn-primary btn-sm" id="26" value="Money">Money</button>
+							<button type="button" class="btn btn-primary btn-sm" id="27" value="Music">Music</button>
+							<button type="button" class="btn btn-primary btn-sm" id="28" value="Office">Office</button>
+							<button type="button" class="btn btn-primary btn-sm" id="29" value="People">People</button>
+							<button type="button" class="btn btn-primary btn-sm" id="30" value="Seasons">Seasons</button>
+							<button type="button" class="btn btn-primary btn-sm" id="31" value="Sports">Sports</button>
+							<button type="button" class="btn btn-primary btn-sm" id="32" value="Time">Time</button>
+							<button type="button" class="btn btn-primary btn-sm" id="33" value="Transportation">Transportation</button>
+							<button type="button" class="btn btn-primary btn-sm" id="34" value="Travel">Travel</button>
+							<button type="button" class="btn btn-primary btn-sm" id="35" value="Weather">Weather</button>
+						</fieldset>
 
-				</div>
-				<!--end div widget-->
+		</div>
+		<!--end div widget-->
 
 				<div class="content">
 					
@@ -164,7 +127,7 @@ $( function() {
 						while($row=mysql_fetch_array($result_set))
 						{
 
-						?>
+					?>
 							<div class="gallery">
 								<figure>
 									<a target="_blank" href="<?php echo $row['file'] ?>">
@@ -177,18 +140,18 @@ $( function() {
 									<div class="name"><?php echo $row['name'] ?></div>
 							</figure>
 							</div>
-						<?php
+					<?php
 						}	
-						?>
+					?>
 
 				</div>
 				<!--end div content-->
 
 	 	</div>
-		<!--end div container->
+		<!--end div container-->
 
   </div>
-	<!end div wrapper->
+	<!--end div wrapper-->
 
 </body>
 </html>
